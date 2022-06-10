2b8316548a4cb7de11ecd40dfcec013540750414
heesu <huisu121@gmail.com>
2022-06-10 19:56:54 +0900

    로그인 모달 css 수정

--- nike-client/src/Components/ModalLogin.js
+++ nike-client/src/Components/ModalLogin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ModalLogin.css';
 
 const ModalLogin = (props) => {
@@ -7,8 +7,33 @@ const ModalLogin = (props) => {
         userid: '',
         password: '',
     };
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
 
-    function loginRequestHandler() {
+
+    const [keepLogin, setKeepLogin] = useState(true);
+
+    function keepLoginHandler() {
+        setKeepLogin(!keepLogin);
+    }
+
+    let idFailureMessage = document.querySelector('.id-failure-message')
+    let passwordFailureMessage = document.querySelector('.password-failure-message')
+
+    function onClickLogin() {
+        if (inputId === '') {
+            idFailureMessage.classList.remove('hide')
+        }
+        if (inputPw === '') {
+            passwordFailureMessage.classList.remove('hide')
+        }
+
     }
 
     return (
@@ -27,29 +52,40 @@ const ModalLogin = (props) => {
                     <input
                         type="text"
                         name="userid"
-                        onChange={(e) => state.userid = e}
-                        placeholder={'아이디'} />
+                        onChange={handleInputId}
+                        placeholder={'아이디'}/>
+                    <div className="id-failure-message hide">필수 입력 항목입니다.</div>
                     <br/>
                     <input
                         type="password"
                         name="password"
-                        onChange={(e) => state.password = e}
+                        onChange={handleInputPw}
                         placeholder={'비밀번호'}/>
+                    <div className="password-failure-message hide">필수 입력 항목입니다.</div>
                 </div>
                 <section className='login__middle'>
-                <div className='login__middle__left'>
-                    <input type="checkbox" name="keepLogin" value="keepLogin" checked />
-                    <label htmlFor="keepLogin">로그인 유지하기</label>
-                </div>
-                <button >아이디/비밀번호 찾기</button>
+                    <div className='login__middle__left'>
+                        <input
+                            type="checkbox"
+                            name="keepLogin"
+                            value="keepLogin"
+                            checked={keepLogin}
+                            onClick={keepLoginHandler}/>
+                        <label htmlFor="keepLogin">로그인 유지하기</label>
+                    </div>
+                    <button className='login__middle__right'>아이디/비밀번호 찾기</button>
                 </section>
-                <div className="header__button">
-                    <button onClick={close}>로그인</button><br/>
-                    <button>카카오계정으로 로그인</button><br/>
-                    <button>페이스북으로 로그인</button>
+                <div className="login__button">
+                    <button className="localLogin" onClick={onClickLogin}>로그인</button>
+                    <br/>
+                    <button className="kakaoLogin">카카오계정으로 로그인</button>
+                    <br/>
+                    <button className="facebookLogin">페이스북으로 로그인</button>
                 </div>
                 <footer className='login__footer'>
-                    {'회원이 아니신가요?'}<button>회원가입</button><br/>
+                    <text>{'회원이 아니신가요? '}</text>
+                    <button>회원가입</button>
+                    <br/>
                     <button>비회원 주문 조회</button>
                 </footer>
             </section>
